6c8d286400df5a56111261d6b4c35bc8401ebed1
qwerty9718 <130994192+qwerty9718@users.noreply.github.com>
2023-09-05 13:55:33 +0500

    Update navbarModule.js

--- resources/js/store/Task/navbarModule.js
+++ resources/js/store/Task/navbarModule.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 export const navbarModule = {
     state: () => ({
-        url:'http://127.0.0.1:8000/api/',
-
+       url:'http://95.130.227.47:81/api/',
     }),
     getters: {
 
